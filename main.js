57b9e07f3b394e227df8af77d34ceacbfb99394d
Mateus Prates <mateusbprates@hotmail.com>
2021-11-29 13:31:20 -0300

    Fazendo a seção de trabalhos recentes

--- main.js
+++ main.js
@@ -7,4 +7,13 @@ window.onload = function digital_fn(){
         collapse.classList.toggle('active')
     })
 
+    let grid = document.querySelector("#site-main .recent-work-area .images-flex")
+    
+    let msnry = new Masonry(grid, {
+
+        itemSelector: '.flex-item',
+        gutter: 100,
+        fitWidth: true,
+    })
 }
+
